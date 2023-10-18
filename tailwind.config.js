07fd226ed5551b35bdd307a09250de34467a6bd6
charly <charly@charlyecho.com>
2023-10-17 23:58:51 -0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.php"],
   theme: {
     extend: {},
   },
---
+++ web/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$files = glob("../files/*.json");
+
+?>
+
+<html lang="en">
+<head>
+    <title>Octopus</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/style.css" rel="stylesheet" />
+</head>
+<body>
+<div class="flex flex-wrap gap-2">
+
+</div>
+</body>
+</html>
