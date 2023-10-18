bc29edeb6927e0fe6826f13ab99ab2667d9ddc56
charly <charly@charlyecho.com>
2023-10-17 23:51:20 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$files = glob("jsons/*.json");
+
+?>
+
+<html lang="en">
+<head>
+    <title>Octopus</title>
+</head>
+<body>
+<div class="flex flex-wrap gap-2">
+
+</div>
+</body>
+</html>
